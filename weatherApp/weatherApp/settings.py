fbc4ab8a3b7d0206ca5becca44101033d869a8ec
syedtibyan <smtt.786@outlook.com>
2022-01-13 07:26:34 +0800

    edit settings.py

--- weatherApp/weatherApp/settings.py
+++ weatherApp/weatherApp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u7q8!ifm5=)d2)@sl2)8z@1e97zeyatbdee#2=1&$h9#zsixeq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
